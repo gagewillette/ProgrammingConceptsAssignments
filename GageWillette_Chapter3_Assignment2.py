d46c1d9ee893a8375fb29472e5ee889218a8a18e
gagewillette <79120652+gagewillette@users.noreply.github.com>
2023-01-23 16:22:10 +0000

    Update 1/23/23

--- GageWillette_Chapter3_Assignment2.py
+++ GageWillette_Chapter3_Assignment2.py
@@ -3,5 +3,41 @@
 # the program with determine the areas of the rectangles
 # and output the greater of the two
 
-x = None
-y = None
+#init variables & set to null
+lengthOne = None
+widthOne = None
+lengthTwo = None
+widthTwo = None
+
+# for later use
+areaOne = None
+areaTwo = None
+
+#create infinite event loop
+while True:
+    #try this code
+    try:
+        # get the measurements for all 4 arguments
+        lengthOne = int(input("Input first length (int only): "))
+        widthOne = int(input("Input first width (int only): "))
+        lengthTwo = int(input("Input second length (int only): "))
+        widthTwo = int(input("Input second width (int only): "))
+        #break loop if no issues
+        break
+    # run this if error
+    except ValueError:
+        print("Please input integer values!")
+        #restart loop
+        continue
+
+areaOne = lengthOne * widthOne
+areaTwo = lengthTwo * widthTwo
+
+print("debug: " , areaOne , " " , areaTwo)
+
+if areaOne > areaTwo:
+    diff = areaOne - areaTwo
+    print(f"The first rect is bigger than the second by {diff} units!")
+else:
+    diff = areaTwo - areaOne
+    print(f"The second rect is bigger than the first by {diff} units!")
