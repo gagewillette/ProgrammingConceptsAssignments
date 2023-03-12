e1acaa8d47433c79cc2906f38427c1f4f2fb249f
gagewillette <gagew0513@gmail.com>
2023-03-12 12:35:39 -0400

    UPDATE CHAPTER 8

--- GageWillette_Chapter8_Assignment1.py
+++ GageWillette_Chapter8_Assignment1.py
@@ -1,12 +1,25 @@
-#Write a program that gets a string containing a person’s first, middle, and last names,
-# and then display their first, middle, and last initials. For example, if the user enters John William Smith the program should display J. W. S.
+#this function accepts an input string of three words. (First Name, Middle Name, Last Name)
+#the function will then output the initials of each of the names
 
+#define main
 def main():
+    #attain user input
     fullName = input("Enter your full name (ex. \"John William Smith\"): ")
+    #split names into name array
     names = fullName.split()
+
+    #check if name array is more than three indicies
+    if len(names) != 3:
+        #raise error if input is wrong
+        raise ValueError("Your name input must be three words! (ex. \"John William Smith\")")
+
+    #accumulator string
     initials = ""
+    #loop thru name array
     for name in names:
+        #add first letter of each name to accumulator string
         initials += str(name[0].capitalize()) + ". " 
+    #print 2 console
     print("Intials: " + initials)
 
 
--- GageWillette_Chapter8_Assignment9.py
+++ GageWillette_Chapter8_Assignment9.py
@@ -1,20 +1,25 @@
-#
+#this file contians three main function (vowelCount, consCount, vowelConsCount) 
+#which will return the number of vowels, consonant, and vowels and consosnts, respectively
+#the main helper function is vowelChecker() which checks if a input character is a vowel
 
+#define main
 def main():
-    
-    #vowelCountInput = input("Enter a string for the vowel count: ")
-    print(vowelCount(input("Enter a string for the vowel count: ")))
-    #consonantCountInput = input("Enter a string for the vowel count: ")
-    print(consonantCount(input("Enter a string for the consonant count: ")))
-    #vowelConsonantCountInput = input("Enter a string for the vowel count: ")
-    print(vowelConsonantCount(input("Enter a string for the vowel and consonant count: ")))
+    #get input for each function and run function
+    vowelCount(input("Enter a string for the vowel count: "))
+    consonantCount(input("Enter a string for the consonant count: "))
+    vowelConsonantCount(input("Enter a string for the vowel and consonant count: "))
 
+#define vowel check w character input paramter
 def vowelChecker(character):
+    #throw error if input is not a single character
     if len(character) > 1:
         raise ValueError("vowelChecker's parameter must be of a single character!")
 
+    #swtich case for character value
     match str(character).lower():
+        #check character against each vowel
         case "a":
+            #return true if vowel
             return True
         case "e":
             return True
@@ -24,33 +29,50 @@ def vowelChecker(character):
             return True
         case "u":
             return True
+    #return false if consonant
     return False
 
+#def vowel count
 def vowelCount(string):
+    #accumulator
     vowelCount = 0
+    #loop thru input string
     for letter in string:
+        #check if vowel
         if vowelChecker(letter):
             vowelCount += 1
 
     return vowelCount
 
 def consonantCount(string):
+    #accumulator
     consCount = 0
+    #loop thru input string
     for letter in string:
+        #check if NOT vowel
         if vowelChecker(letter) == False:
             consCount += 1
 
     return consCount
 
 def vowelConsonantCount(string):
+    #accumulator
     consCount = 0
+    #accumulator
     vowelCount = 0
+    #loop thru input string
     for letter in string:
+        #check if vowel
         if vowelChecker(letter):
+            #add if vowel
             vowelCount += 1
+            #return to top
             continue
+        #add if not vowel
         consCount += 1
 
-    return ["Vowels: " + str(vowelCount), "Consonants: " + str(consCount)]
+    #return array of vowelCount and consCount
+    return ["Vowel: " + str(vowelCount), "Consonants: " + str(consCount)]
 
+#entrance point
 main()
